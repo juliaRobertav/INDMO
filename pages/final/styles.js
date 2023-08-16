a4b064c843bfd01ccc1311b220debe6a5a72745a
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-08-16 16:50:15 -0300

    Add files via upload

---
+++ pages/final/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#a7c1eb',
+        alignItems:'center',
+        justifyContent:'center',
+        flex:1,
+    },
+
+    txt:{
+        fontSize:30,
+    },
+
+})
+
+export default styles
---
+++ pages/home/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#a7c1eb',
+        alignItems:'center',
+        justifyContent:'center',
+        flex:1,
+    },
+
+    txt:{
+        fontSize:30,
+    },
+    caixa:{
+        fontSize: 10
+    },
+
+})
+
+export default styles
