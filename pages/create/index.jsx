dd7b2f6a2e1594604a237d539c3feeb0e83cf05b
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-09-06 16:48:44 -0300

    Add files via upload

---
+++ pages/create/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text} from 'react-native'
+import styles from './styles'
+
+export default function Create(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>Create</Text>
+        </View>
+    )
+}
